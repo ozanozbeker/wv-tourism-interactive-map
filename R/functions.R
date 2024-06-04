70a9aca58922b19facfd89b6a0e67747cc15fc17
Ozan Ozbeker <ozanozbeker99@outlook.com>
2024-06-03 23:29:05 -0400

    Converted to iterative resp + added google api

--- R/functions.R
+++ R/functions.R
@@ -1,56 +1,154 @@
-# Import ----
-## Logging ----
-write_log_line = function(path_log) {
-  write_file( # I like the lines, sue me
-    str_c('|------------------------------------------------------------------------|\n'),
-    path_log,
-    append = TRUE
-  )
-}
-
-write_to_log = function(path_log, text) {
-  write_file(
-    str_c(str_sub(now(), 1, 23),' | ', text, '\n'),
-    path_log,
-    append = TRUE
-  )
-}
-
-## Scraping ----
+# httr2 ----
 get_request = function(url) {
+  require(httr2)
+
   request(url) |>
     req_method("GET") |>
     req_throttle(rate = 30 / 60) |> # 30 requests per minute
     req_user_agent("Ozan Ozbeker (https://github.com/ozanozbeker/wv-tourism)")
 }
 
-extract_sitemap_index = function(resp_sitemap_index) {
-  # There might be a cleaner way of doing this with the
-  # actual xml2 functions but {tidyr} go burrr.
-  resp_body_xml(resp_companies_index) |>
-  xml2::as_list() |>
-  as_list() |>
-  list_flatten() |>
-  list_flatten() |>
-  list_flatten() |>
-  as_tibble(.name_repair = janitor::make_clean_names) |>
-  rename(
-    sitemapindex_sitemap_loc_1 = sitemapindex_sitemap_loc,
-    sitemapindex_sitemap_lastmod_1 = sitemapindex_sitemap_lastmod) |>
-  pivot_longer(
-    cols = everything(),
-    names_to = c(".value", "row"),
-    names_pattern = "sitemapindex_sitemap_(.*)_(.*)") |>
-  mutate(
-    url = loc,
-    last_modified = ymd_hms(lastmod, tz = "UTC"),
-    .keep = "none")
+extract_links = function(resp) {
+  resp_body_html(resp) |>
+    html_element("ul.ttd") |>
+    html_children() |>
+    html_children() |>
+    html_attrs() |>
+    as_tibble_col() |>
+    unnest_wider(value) |>
+    clean_names() |>
+    filter(is.na(class)) |>
+    select(category = title, url = href, url_bg = data_bg)
 }
 
+extract_listings = function(category, resp) {
+
+  base = resp |>
+    resp_body_html() |>
+    html_element(".listings-wall") |>
+    html_children()
+
+  data_1 = base |>
+    html_attrs() |>
+    as_tibble_col() |>
+    unnest_wider(value) |>
+    clean_names() |>
+    select(-class) |>
+    remove_empty("rows") |>
+    rename(city = data_city, region = data_region, county = data_county) |>
+    mutate(across(everything(), str_to_title))
+
+  data_2 = base |>
+    html_children() |>
+    html_children() |>
+    html_elements("div.meta-top") |>
+    html_children() |>
+    html_attrs() |>
+    as_tibble_col() |>
+    unnest_wider(value) |>
+    clean_names() |>
+    select(title, href) |>
+    mutate(
+      source = if_else(str_detect(href, "wvtourism"), "wvtourism", "google"),
+      title = str_remove_all(title, "LIST: |MAPS: ")) |>
+    pivot_wider(
+      names_from = source,
+      names_prefix = "url_",
+      values_from = href,
+      values_fn = list) |>
+    unnest_longer(col = everything()) |>
+    rename(company = title) |>
+    mutate(address = str_sub(url_google, 28L, -1L)) |>
+    select(-url_google)
+
+  data = data_2 |>
+    bind_cols(data_1) |>
+    mutate(category = category, .before = company)
+
+  return(data)
+}
+
+get_google_details = function(search_text) {
+  require(httr2)
+  require(keyring)
+
+  request("https://places.googleapis.com/v1/places:searchText") |>
+    req_method("POST") |>
+    req_throttle(rate = 30 / 60) |> # limit 30 requests per minute
+    req_user_agent("Ozan Ozbeker (https://github.com/ozanozbeker/wv-tourism)") |>
+    req_headers(
+      "Content-Type" = "application/json",
+      "X-Goog-Api-Key" = key_get("Google Maps API Key"),
+      "X-Goog-FieldMask" = str_c(
+        "places.id",
+        "places.nationalPhoneNumber",
+        "places.formattedAddress",
+        "places.location",
+        "places.rating",
+        "places.googleMapsUri",
+        "places.websiteUri",
+        "places.regularOpeningHours",
+        "places.displayName",
+        "places.primaryTypeDisplayName",
+        "places.paymentOptions",
+        "places.accessibilityOptions",
+        "places.generativeSummary",
+        sep = ",")) |>
+    req_body_json(list(textQuery = search_text))
+}
+
+extract_google_details = function(url_wvtourism, resp) {
+  base = resp_body_json(resp) |>
+    list_flatten() |>
+    list_flatten()
+
+  data = tibble(
+    id = base |> pluck("places_id"),
+    name = base |> pluck("places_displayName", "text"),
+    description = base |> pluck("places_primaryTypeDisplayName", "text"),
+    summary = base |> pluck("places_generativeSummary", "overview", "text"),
+    phone = base |> pluck("places_nationalPhoneNumber"),
+    address = base |> pluck("places_formattedAddress"),
+    latitude = base |> pluck("places_location", "latitude"),
+    longitude = base |> pluck("places_location", "longitude"),
+    rating = base |> pluck("places_rating"),
+    url_googlemaps = base |> pluck("places_googleMapsUri"),
+    url_company = base |> pluck("places_websiteUri"),
+    is_wheelchair_accessible = base |> pluck("places_accessibilityOptions", "wheelchairAccessibleParking"))
+
+  payment = base |>
+    pluck("places_paymentOptions") |>
+    list_transpose() |>
+    as_tibble_col() |>
+    unnest_wider(value) |>
+    clean_names()
+
+  hours = base |>
+    pluck("places_regularOpeningHours", "weekdayDescriptions") |>
+    as_tibble_col("hours") |>
+    unnest(hours) |>
+    mutate(
+      day = str_extract(hours, "[:alpha:]*"),
+      hours = str_extract(hours, "\\d.*")) |>
+    pivot_wider(names_from = day, names_prefix = "hours_", values_from = hours) |>
+    clean_names()
+
+  data_all = data |>
+    bind_cols(payment) |>
+    bind_cols(hours) |>
+    mutate(url_wvtourism)
+
+  return(data_all)
+}
+
+# Tidy ----
 extract_after_last_comma = function(address_clean) {
+  require(tidyverse)
+  require(janitor)
+
   last_comma = address_clean |>
     str_locate_all(",") |>
-    as_tibble(.name_repair = janitor::make_clean_names) |>
+    as_tibble(.name_repair = make_clean_names) |>
     slice_tail(n = 1) |>
     pull(1)
 
@@ -58,20 +156,19 @@ extract_after_last_comma = function(address_clean) {
     text = address_clean |>
       str_sub(last_comma[[1]] + 2, -1) |>
       str_squish()
-
     return(text)
-
   }, error = function(e) {
-
     return(NA_character_)
-
   })
 }
 
 extract_before_last_comma = function(address_clean) {
+  require(tidyverse)
+  require(janitor)
+
   last_comma = address_clean |>
     str_locate_all(",") |>
-    as_tibble(.name_repair = janitor::make_clean_names) |>
+    as_tibble(.name_repair = make_clean_names) |>
     slice_tail(n = 1) |>
     pull(1)
 
@@ -79,12 +176,8 @@ extract_before_last_comma = function(address_clean) {
     text = address_clean |>
       str_sub(1, last_comma[[1]] - 1) |>
       str_squish()
-
     return(text)
-
   }, error = function(e) {
-
     return(address_clean)
-
   })
 }
--- R/scrape_listings.R
+++ R/scrape_listings.R
@@ -3,173 +3,84 @@ library(tidyverse) # Load the Tidyverse
 library(httr2) # HTTP Requests
 library(rvest) # Parse HTML
 library(janitor) # Cleaning Dirty Data
+library(keyring) # Access to OS keyring
 source("R/functions.R") # Project-wide functions
+source("R/variables.R") # Project-wide variables
 
-# Functions ----
-extract_links = function(resp) {
-  resp_body_html(resp) |>
-  html_element("ul.ttd") |>
-  html_children() |>
-  html_children() |>
-  html_attrs() |>
-  as_tibble_col() |>
-  unnest_wider(value) |>
-  clean_names() |>
-  filter(str_detect(href, "things-to-do")) |>
-  filter(is.na(class)) |>
-  select(category = title, url = href, url_bg = data_bg)
-}
+# Scrape ----
+urls = c(
+  "https://wvtourism.com/things-to-do/outdoor-adventures/",
+  "https://wvtourism.com/things-to-do/luxury-relaxation/",
+  "https://wvtourism.com/things-to-do/arts-culture-history/",
+  "https://wvtourism.com/places-to-stay/",
+  "https://wvtourism.com/places-to-go/")
 
-extract_listing = function(category, url) {
-  resp = get_request(url) |> req_perform()
+links_responses = urls |>
+  map(get_request) |>
+  req_perform_sequential(on_error = "continue")
 
-  listings = resp |>
-    resp_body_html() |>
-    html_element(".listings-wall") |>
-    html_children()
-
-  listings_details_1 = listings |>
-    html_attrs() |>
-    as_tibble_col() |>
-    unnest_wider(value) |>
-    clean_names() |>
-    select(-class) |>
-    remove_empty("rows") |>
-    rename(city = data_city, region = data_region, county = data_county) |>
-    mutate(across(everything(), str_to_title))
-
-  listings_details_2 = listings |>
-    html_children() |>
-    html_children() |>
-    html_elements("div.meta-top") |>
-    html_children() |>
-    html_attrs() |>
-    as_tibble_col() |>
-    unnest_wider(value) |>
-    clean_names() |>
-    select(title, href) |>
-    mutate(
-      source = if_else(str_detect(href, "wvtourism"), "wvtourism", "google"),
-      title = str_remove_all(title, "LIST: |MAPS: ")) |>
-    pivot_wider(
-      names_from = source,
-      names_prefix = "url_",
-      values_from = href,
-      values_fn = list) |>
-    unnest_longer(col = everything()) |>
-    rename(company = title) |>
-    mutate(
-      address = str_sub(url_google, 28L, -1L),
-      url_google = URLencode(url_google))
-
-  # Extracting the phone number is easy, but the inconsistency of the phone number
-  # for the same company across different listings is not worth the hassel.
-  # Also phone numbers are very liable to change anyway.
-  # listings_details_3 = listings |>
-  #   html_children() |>
-  #   html_children() |>
-  #   html_elements("div.meta-bottom") |>
-  #   html_children() |>
-  #   html_elements('a') |>
-  #   html_text2() |>
-  #   as_tibble_col() |>
-  #   mutate(phone = str_sub(value, 8L, -1L), .keep = "none")
-
-  data = listings_details_2 |>
-    bind_cols(listings_details_1) |>
-    # bind_cols(listings_details_3) |>
-    mutate(category = category, .before = company)
-
-  return(data)
-}
-
-# Scrapes ----
-# The steps are pretty much the same for the most part but wanted to
-# separate as they are different calls and have some minor variation.
-
-# Zip Codes
-zips = read_html("https://www.zipcodestogo.com/West%20Virginia/") |>
-  html_element("table.inner_table") |>
-  html_table() |>
-  select(zip = X1, city = X2, county = X3) |>
-  mutate(zip = parse_number(zip)) |>
-  filter(!is.na(zip)) |>
-  suppressWarnings() # intentional warning parsing the first two rows
-
-# Outdoor Adventures
-resp_outdoor = get_request("https://wvtourism.com/things-to-do/outdoor-adventures/") |> req_perform()
-resp_outdoor
-
-links_outdoor = extract_links(resp_outdoor)
-
-outdoor = links_outdoor |>
-  select(category, url) |>
-  pmap(extract_listing, .progress = TRUE) |>
+links = resps_successes(links_responses) |>
+  map(extract_links) |>
   list_rbind() |>
-  mutate(group = "Outdoor Adventures", .before = category)
+  slice_head(n = 1, by = url) |> # Some Categories are repeated across website sections
+  filter(
+    category != "Stargazing", # Section of website unfinished as of 2024-06-03
+    # The following are big/different enough to need their own scripts:
+    str_detect(url, "/state-parks/", negate = TRUE),    # scrape_state-parks.R
+    str_detect(url, "/scenic-routes/", negate = TRUE),  # scrape_routes.R
+    str_detect(url, "/travel-regions/", negate = TRUE)) # scrape_cities.R
 
-# Luxury Relaxation
-resp_luxury = get_request("https://wvtourism.com/things-to-do/luxury-relaxation/") |> req_perform()
-resp_luxury
+listings_responses = links |>
+  pull(url) |>
+  map(get_request) |>
+  req_perform_sequential(on_error = "continue")
 
-links_luxury = extract_links(resp_luxury)
+listings = list(category = links$category, resp = listings_responses) |>
+  pmap(extract_listings) |>
+  list_rbind()
 
-luxury = links_luxury |>
-  select(category, url) |>
-  pmap(extract_listing, .progress = TRUE) |>
-  list_rbind() |>
-  mutate(group = "Luxury Relaxation", .before = category)
+# Clean ----
+# There is redundancy in scrape data collection, this is on purpose.
+# The scrape collects the HTML data regardless, so even though I'm using my custom data to fill,
+# the scrape data is there as a back up if needed.
 
-# Arts, Culture, & History
-resp_arts = get_request("https://wvtourism.com/things-to-do/arts-culture-history/") |> req_perform()
-resp_arts
-
-links_arts = extract_links(resp_arts)
-
-arts = links_arts |>
-  # Scenic Routes are non-standard and will in a different script
-  filter(url != "https://wvtourism.com/things-to-do/arts-culture-history/scenic-routes/") |>
-  select(category, url) |>
-  pmap(extract_listing, .progress = TRUE) |>
-  list_rbind() |>
-  mutate(group = "Arts, Culture, & History", .before = category)
-
-# Combine & Clean Data ----
-# There is redundancy in data collection, this is on purpose.
-# The scrape collects the HTML data regardless, so even though I'm using my custom data
-# to fill, the scrape data is there as a back up if needed.
-
-data = outdoor |>
-  bind_rows(luxury) |>
-  bind_rows(arts) |>
+data = listings |>
   mutate(
     across(everything(), \(col) if_else(col == "", NA_character_, str_squish(col))),
     region = if_else(region == "Array", NA_character_, region),
-    zip = if_else(str_sub(address, -5L, -5L) == "-", str_sub(address, -10L, -6L), str_sub(address, -5L, -1L))) |>
-  left_join(zips, join_by(zip)) |>
+    zip = if_else(
+      str_sub(address, -5L, -5L) == "-",
+      str_sub(address, -10L, -6L),
+      str_sub(address, -5L, -1L))) |>
+  left_join(zips |> select(-state, -country, -irs_estimated_population), join_by(zip)) |>
   mutate(
     city = coalesce(city.y, city.x),
     county = coalesce(county.y, county.x),
     .keep = "unused") |>
   left_join(travel_regions, join_by(county)) |>
   mutate(region = coalesce(region.y, region.x), .keep = "unused") |>
-  select(
-    group, category, company, phone, address, city, county,
-    region, state, country, zip, url_wvtourism, url_google
-  )
+  select(category, company, address, city, county, region, zip, url_wvtourism)
 
 companies = data |>
-  select(-group, -category) |>
+  select(-category) |>
   distinct() |>
   arrange(company) |>
-  slice_head(n = 1, by = company) # There were only 3 duplicates anyway
+  slice_head(n = 1, by = company) # There were 3 duplicates
 
-tags = data |>
-  select(group, category, url_wvtourism) |>
-  inner_join(companies |> select(url_wvtourism), join_by(url_wvtourism)) |>
-  arrange(url_wvtourism) |>
-  mutate(topic = "Things To Do", .before = group)
+categories = data |>
+  select(category, url_wvtourism) |>
+  inner_join(companies |> select(url_wvtourism), join_by(url_wvtourism))
 
-# Export ----
-write_csv(companies, "Output/companies.csv")
-write_csv(tags, "Output/tags.csv")
+write_rds(companies, "Pipeline/companies.rds")
+write_rds(categories, "Pipeline/categories.rds")
+
+# Google Maps Data ----
+google_details_responses = companies |>
+  mutate(search_text = str_c(company, address, sep = " ")) |>
+  pull(search_text) |>
+  map(get_google_details) |>
+  req_perform_sequential(on_error = "continue")
+
+google_details = list(url_wvtourism = companies$url_wvtourism, resp = google_responses) |>
+  pmap(extract_google_details) |>
+  list_rbind()
